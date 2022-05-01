ad2d718cd926c97d7987627940f7ef711185c74f
prplblck <kariurek@gmail.com>
2022-05-01 23:19:55 +0200

    context base

---
+++ src/store/cart-provider.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react';
+
+const DEFAULT_CART = { items: [], totalPrice: 0,totalItems: 0 };
+
+const CartContext = React.createContext();
+
+const cartReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD': {
+			const { item } = action;
+      if (state.items.inde)
+			return;
+		}
+		case 'REMOVE': {
+			return;
+		}
+		default: {
+			return state;
+		}
+	}
+};
+
+const CartProvider = ({ children }) => {
+	const [cartState, cartDispatch] = useReducer(cartReducer, DEFAULT_CART);
+
+	const value = { cartState, cartDispatch };
+
+	return (
+		<CartContext.Provider value={value}>{children}</CartContext.Provider>
+	);
+};
+
+export { CartProvider };
